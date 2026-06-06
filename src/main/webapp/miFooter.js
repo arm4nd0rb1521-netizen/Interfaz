7a34ef39472fb764b7c742eb5297e30574403e3e
Reyes Becerra Cesar Armando <arm4nd0rb1521@gmail.com>
2026-06-05 23:07:45 -0600

    Comienzo del caos

---
+++ src/main/webapp/miFooter.js
@@ -0,0 +1,13 @@
+class CrearFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                <div class='contenido'>
+                    
+                </div>
+            </footer>
+        
+        `;
+    }
+}
+
--- src/main/webapp/miHeader.js
+++ src/main/webapp/miHeader.js
@@ -3,17 +3,24 @@ class CrearHeader extends HTMLElement {
         this.innerHTML = `
             <header>
                 <div class="principal">
+                        <img src='logo.png' class='logo' alt='logo' width='100px' height='100px'>
+                </div>
+                <div class='nave'>
                     <nav class="navegacion">
                         <a href="IHome.html">Home</a>
                         <a href="IVentas.html">Ventas</a>
-                        <a href="ILog.html">Logistica</a>
-                        <a href="IAdmin.html">Administracion</a>
+                        <a href="IVentas.html">Reportes</a>
+                        <a href="ILog.html">Proveedores</a>
+                        <a href="ILog.html">Compras</a>
+                        <a href="IAdmin.html">Movimientos de stock</a>
+                        <a href="IAdmin.html">Detalles de compra</a>
+                        <a href="IAdmin.html">Detalles de venta</a>
+                        <a href="IAdmin.html">Usuarios</a>
+                        <a href="IAdmin.html">Roles y permisos</a>
+                        <a href="IAdmin.html">Productos</a>
+                        <a href="IAdmin.html">Categorias de productos</a>
 
                     </nav>
-
-                    <div class="nombre">
-                        <h3>My Own Shop</h3>
-                    </div>
                 </div>
             </header>
             `;
